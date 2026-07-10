0744d457c6e49468e6fc4cecd55d494f347931ec
allykeightley <allysonkeightley@outlook.com>
2026-07-10 16:47:53 -0700

    Initial Commit

---
+++ hack-iosTests/hack_iosTests.swift
@@ -0,0 +1,19 @@
+//
+//  hack_iosTests.swift
+//  hack-iosTests
+//
+//  Created by Ally Keightley on 7/10/26.
+//
+
+import Testing
+@testable import hack_ios
+
+struct hack_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
